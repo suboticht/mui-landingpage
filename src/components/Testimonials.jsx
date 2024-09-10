b5bd772e3a12a4c313bc8641bc8513214596eca8
suboticht <86337073+suboticht@users.noreply.github.com>
2024-09-10 09:22:59 +0700

    Add files via upload

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,42 +11,42 @@ import { useTheme } from '@mui/system';
 
 const userTestimonials = [
   {
-    avatar: <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />,
+    avatar: <Avatar alt="Remy Sharp" src="" />,
     name: 'Remy Sharp',
     occupation: 'Senior Engineer',
     testimonial:
       "I absolutely love how versatile this product is! Whether I'm tackling work projects or indulging in my favorite hobbies, it seamlessly adapts to my changing needs. Its intuitive design has truly enhanced my daily routine, making tasks more efficient and enjoyable.",
   },
   {
-    avatar: <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />,
+    avatar: <Avatar>T</Avatar>,
     name: 'Travis Howard',
     occupation: 'Lead Product Designer',
     testimonial:
       "One of the standout features of this product is the exceptional customer support. In my experience, the team behind this product has been quick to respond and incredibly helpful. It's reassuring to know that they stand firmly behind their product.",
   },
   {
-    avatar: <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />,
+    avatar: <Avatar>C</Avatar>,
     name: 'Cindy Baker',
     occupation: 'CTO',
     testimonial:
       'The level of simplicity and user-friendliness in this product has significantly simplified my life. I appreciate the creators for delivering a solution that not only meets but exceeds user expectations.',
   },
   {
-    avatar: <Avatar alt="Remy Sharp" src="/static/images/avatar/4.jpg" />,
+    avatar: <Avatar>J</Avatar>,
     name: 'Julia Stewart',
     occupation: 'Senior Engineer',
     testimonial:
       "I appreciate the attention to detail in the design of this product. The small touches make a big difference, and it's evident that the creators focused on delivering a premium experience.",
   },
   {
-    avatar: <Avatar alt="Travis Howard" src="/static/images/avatar/5.jpg" />,
+    avatar: <Avatar>J</Avatar>,
     name: 'John Smith',
     occupation: 'Product Designer',
     testimonial:
       "I've tried other similar products, but this one stands out for its innovative features. It's clear that the makers put a lot of thought into creating a solution that truly addresses user needs.",
   },
   {
-    avatar: <Avatar alt="Cindy Baker" src="/static/images/avatar/6.jpg" />,
+    avatar: <Avatar>D</Avatar>,
     name: 'Daniel Wolf',
     occupation: 'CDO',
     testimonial:
